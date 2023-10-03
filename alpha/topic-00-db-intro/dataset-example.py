d998c211539fe0d7153c40410907e58d5ba631a7
akhilpathi <75804264+akhilpathi@users.noreply.github.com>
2023-10-04 05:06:05 +0530

    Delete alpha directory

--- alpha/topic-00-db-intro/dataset-example.py
+++
@@ -1,9 +0,0 @@
-import dataset
-
-db = dataset.connect('sqlite:///pets.db')
-
-table = db['pet']
-data = list(table.find())
-data = [dict(item) for item in data]
-# data = [dict(item) for item in table.find_one()]
-print(data)
